5781ec487bd9080d964f3a7d7f9e726a98007105
PatrickChow0803 <patrickchow0803@gmail.com>
2024-10-22 14:24:00 -0400

    Refactored AppScaffold

--- app/src/main/java/com/patrickchow/financeforsuccess/ui/common/AppScaffold.kt
+++ app/src/main/java/com/patrickchow/financeforsuccess/ui/common/AppScaffold.kt
@@ -1,16 +1,25 @@
 package com.patrickchow.financeforsuccess.ui.common
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.patrickchow.financeforsuccess.ui.extensions.customScrollableColumn
 
 @Composable
 fun AppScaffold(
     title: String,
     navController: NavController,
-    content: @Composable (PaddingValues) -> Unit
+    content: @Composable ColumnScope.(PaddingValues) -> Unit
 ) {
+    val scrollState = rememberScrollState()
+
     Scaffold(
         topBar = {
             CustomAppBar(
@@ -19,6 +28,14 @@ fun AppScaffold(
             )
         }
     ) { innerPadding ->
-        content(innerPadding)
+        Column(
+            modifier = Modifier
+                .customScrollableColumn(scrollState, innerPadding),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
+        ) {
+            content(innerPadding) // Pass the padding to the content inside the column
+        }
     }
 }
+
